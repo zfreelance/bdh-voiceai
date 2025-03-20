d5b12c1ef728bf41fb67de83d5e450f7994b7965
zNadir <115375151+znadir@users.noreply.github.com>
2025-03-20 13:36:04 -0400

    test env vars

--- api/utils.php
+++ api/utils.php
@@ -1,5 +1,15 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php'; // Adjust path if needed
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../'); // Adjust path if needed
+$dotenv->load();
+
+echo "App Name: " . $_ENV['APP_NAME'] . "<br>";
+
+
 function getTimestamp()
 {
     return date("Y-m-d H:i:s");
