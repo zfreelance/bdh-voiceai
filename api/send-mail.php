edd1dcc3327ab1d57d432fde8c643756f0e064a5
zNadir <115375151+znadir@users.noreply.github.com>
2025-03-20 13:54:50 -0400

    send-mail

--- api/send-mail.php
+++ api/send-mail.php
@@ -1,14 +1,63 @@
 <?php
 
-if (!isset($_POST['fromEmail'], $_POST['fromName'], $_POST['emailSubject'], $_POST['emailHtml'])) {
-    die("Error: Missing required fields.");
-}
+require 'utils.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+// env vars
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$MJ_APIKEY_PUBLIC = $_ENV['MJ_APIKEY_PUBLIC'];
+$MJ_APIKEY_PRIVATE = $_ENV['MJ_APIKEY_PRIVATE'];
 
 
-$from_email = $_POST['fromEmail'];
-$from_name = $_POST['fromName'];
-$email_subject = $_POST['emailSubject'];
-$email_html = $_POST['emailHtml'];
+//
 
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+
+$required_fields = ['toEmail', 'fromEmail', 'fromName', 'emailSubject', 'emailHtml'];
+
+check_required($data, $required_fields);
+
+$to_email = $data['toEmail'];
+$from_email = $data['fromEmail'];
+$from_name = $data['fromName'];
+$email_subject = $data['emailSubject'];
+$email_html = $data['emailHtml'];
+
+$url = "https://api.mailjet.com/v3.1/send";
+
+$data = [
+    "Messages" => [
+        [
+            "From" => ["Email" => $from_email, "Name" => $from_name],
+            "To" => [["Email" => $to_email]],
+            "Subject" => $email_subject,
+            "HTMLPart" => $email_html
+        ]
+    ]
+];
+
+$headers = [
+    "Content-Type: application/json"
+];
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_USERPWD, "$MJ_APIKEY_PUBLIC:$MJ_APIKEY_PRIVATE");
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+$response = curl_exec($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+return ["status" => $http_code, "response" => json_decode($response, true)];
 
 ?>
--- api/utils.php
+++ api/utils.php
@@ -5,5 +5,16 @@ function getTimestamp()
     return date("Y-m-d H:i:s");
 }
 
+// check required vars
+function check_required($data, $required_fields)
+{
+    foreach ($required_fields as $field) {
+        if (empty($data[$field])) {
+            http_response_code(400);
+            die(json_encode(["error" => "Missing required field - $field."]));
+        }
+    }
+}
+
 
 ?>
